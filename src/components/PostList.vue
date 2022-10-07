097cf862b1bc5387fa92d682c6b73dbf6b9b39ac
Aleksandr <it.kucherov@gmail.com>
2022-10-07 10:18:36 +0300

    условный рендеринг

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,9 @@
 <template>
-	<div>
-		<h2>Список пользователей</h2>
+	<div v-if="posts.length > 0">
+		<h2>Список постов</h2>
 		<PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
 	</div>
+	<h2 v-else>Список постов пуст</h2>
 </template>
 <script>
 import PostItem from './PostItem.vue';
