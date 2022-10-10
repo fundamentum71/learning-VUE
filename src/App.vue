706e093559813b8244859b68ad2a48940aea4968
Aleksandr <it.kucherov@gmail.com>
2022-10-10 10:39:10 +0300

    реализация ленты прогрузки

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,9 @@
 
 		<PostList :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading" />
 		<h3 v-else>Идет загрузка...</h3>
+		<div ref="observer" class="observe"></div>
 
-		<div class="page__wrapper">
+		<!--<div class="page__wrapper">
 			<div
 				v-for="pageNumber in totalPages"
 				:key="pageNumber"
@@ -26,7 +27,7 @@
 			>
 				{{ pageNumber }}
 			</div>
-		</div>
+		</div>-->
 	</div>
 </template>
 
@@ -67,9 +68,9 @@ export default {
 		showDialog() {
 			this.dialogVisible = true;
 		},
-		changePage(pageNumber) {
-			this.page = pageNumber;
-		},
+		//changePage(pageNumber) {
+		//	this.page = pageNumber;
+		//},
 		async fetchPosts() {
 			try {
 				this.isPostLoading = true;
@@ -87,10 +88,38 @@ export default {
 				this.isPostLoading = false;
 			}
 		},
+		async loadMorePosts() {
+			try {
+				this.page += 1;
+
+				const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`, {
+					params: {
+						_page: this.page,
+						_limit: this.limit,
+					},
+				});
+				this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+				this.posts = [...this.posts, ...response.data];
+			} catch (e) {
+				alert('error');
+			}
+		},
 	},
 	components: { PostForm, PostList, PostForm },
 	mounted() {
 		this.fetchPosts();
+		let options = {
+			rootMargin: '0px',
+			threshold: 1.0,
+		};
+
+		let callback = (entries, observer) => {
+			if (entries[0].isIntersecting && this.page < this.totalPages) {
+				this.loadMorePosts();
+			}
+		};
+		let observer = new IntersectionObserver(callback, options);
+		observer.observe(this.$refs.observer);
 	},
 	computed: {
 		sortedPosts() {
@@ -105,9 +134,9 @@ export default {
 		},
 	},
 	watch: {
-		page() {
-			this.fetchPosts();
-		},
+		//page() {
+		//	this.fetchPosts();
+		//},
 	},
 };
 </script>
@@ -143,5 +172,9 @@ export default {
 }
 .current-page {
 	border: 2px solid teal;
+}
+.observe {
+	height: 30px;
+	background: green;
 }
 </style>
